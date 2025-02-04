aed50710159b791d1ffda29a70aba4242494e012
Vorn-Hagen <thibaut.francois@edu.devinci.fr>
2025-02-04 16:35:03 +0100

    :tada:

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const ip  = require('ip');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+const ipAdress = ip.address();  
+
+
+app.get('/', (req, res) => {
+    //send ip address to client
+    res.send(ipAdress);
+});
 
 io.on('connection', (socket) => {
     console.log('Un client est connecté');
@@ -22,5 +30,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(3000, () => {
-    console.log('Serveur Socket.IO en écoute sur http://localhost:3000');
+    console.log('Serveur Socket.IO en écoute http://'+ipAdress+':3000');
 });
