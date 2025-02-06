8d3d8d90288da897bd3c26bf7ba96fe0bf050380
Vorn-Hagen <thibaut.francois@edu.devinci.fr>
2025-02-06 14:56:01 +0100

    :tada: pls work properly

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ const server = http.createServer(app);
 const io = socketIo(server);
 const ipAdress = ip.address();  
 
+// node script.js
+
+
+// Stocker les clients connectés par type
+const clients = {
+    clientTablette: null,
+    clientServoMoteur: null
+};
 
 app.get('/', (req, res) => {
     //send ip address to client
@@ -22,10 +30,47 @@ io.on('connection', (socket) => {
         console.log('Message reçu du client:', data);
         // Envoyer une réponse au client
         socket.emit('response', { data: 'Message bien reçu par le serveur Node.js!' });
+
     });
+
+    socket.on('register', (clientType) => {
+        if (clientType === 'clientTablette' || clientType === 'clientServoMoteur') {
+            clients[clientType] = socket; // Stocker la référence du client
+            console.log(`Client enregistré : ${clientType} (${socket.id})`);
+        }
+    });
+
+    // Écouter les messages de clientTablette et les envoyer à clientServoMoteur
+    socket.on('commandeCasier', (data) => {
+        console.log('Commande reçue de clientTablette:', data);
+    
+        // Rechercher un client ServoMoteur et lui envoyer la commande
+        // console.log(clients.clientServoMoteur);
+        // io.sockets.sockets.forEach((client) => {
+        //     //console.log(client.clientType);
+        //     if (client.clientType === 'clientServoMoteur') {
+        //         client.emit('commandeCasier', data);
+        //         console.log('Commande envoyée à clientServoMoteur');
+        //     }
+        // });
+
+        if (clients.clientServoMoteur){
+            clients.clientServoMoteur.emit('commandeCasier', data);
+            console.log('CCommande envoyée à clientServoMoteur');
+        }
+    });
+
 
     socket.on('disconnect', () => {
         console.log('Un client s\'est déconnecté');
+
+         // Supprimer le client de la liste s'il se déconnecte
+        Object.keys(clients).forEach((key) => {
+            if (clients[key] && clients[key].id === socket.id) {
+                clients[key] = null;
+                console.log(`Client ${key} supprimé de la liste`);
+            }
+        });
     });
 });
 
